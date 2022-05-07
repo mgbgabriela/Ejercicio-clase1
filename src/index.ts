3853928d61e09fab4c0988969a9c5c80b9a80f98
Gabriela <104107313+mgbgabriela@users.noreply.github.com>
2022-05-06 23:07:55 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+let dato1: string =
+  "Como poder ver los repositorios desde CodeSandbox (que no funciona igual que el del profesor";
+
+console.log(dato1);
+
+let dato2: string = "Entrar en Codesandbox.io con la cuenta de Github.com";
+
+console.log(dato2);
+
+let dato3: string = "Crear un repositorio";
+
+console.log(dato3);
+
+let dato4: string = "Comitearlo";
+
+console.log(dato4);
+
+let dato5: string = "Ingresar en Github.com";
+
+console.log(dato5);
+
+let dato6: string = "Copiar el enlace del repositorio recién comiteado";
+
+console.log(dato6);
+
+let dato7: string = "Volver a CodeSandbox";
+
+console.log(dato7);
+
+let dato8: string = "Ir a repositorio";
+
+console.log(dato8);
+
+let dato9: string = "importar nuevo repositorio";
+
+console.log(dato9);
+
+let dato10: string = "REalizarle una modificación al archivo";
+
+console.log(dato10);
+
+let dato11: string = "Comitearlo nuevamente";
+
+console.log(dato11);
+
+let dato12: string = "Probar si funciona";
+
+console.log(dato12);
